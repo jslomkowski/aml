143201dbb1f9fc640bffed28c4ae81947d367fe5
Jerzy Slomkowski <jerzy.slomkowski@t-mobile.pl>
2021-08-09 15:15:27 +0200

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
 
+def list_reqs(fname='requirements.txt'):
+    with open(fname) as fd:
+        return fd.read().splitlines()
+
+
 setup(
     name='aml',
     version='0.1',
     description='xtreme gridsearchCV',
     author='Jerzy Słomkowski',
-    packages=find_packages(exclude=['tests'])
+    packages=find_packages(exclude=['tests']),
+    install_requires=list_reqs()
+
 )
